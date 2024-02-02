62cd421082f549c7170549a0f77349cfe4ba634d
jhs512 <jangka512@gmail.com>
2024-02-02 20:15:24 +0900

    work

---
+++ src/main/java/com/ll/auth2024/Auth2024Application.java
@@ -0,0 +1,13 @@
+package com.ll.auth2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auth2024Application.class, args);
+    }
+
+}
